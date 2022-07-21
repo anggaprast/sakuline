1ef2e0c825edcf76b0405f967647274e2d305fc5
Angga Pra <anggaprastyo010@gmail.com>
2022-07-21 12:18:17 +0700

    Update Auth.php

--- controllers/Auth.php
+++ controllers/Auth.php
@@ -173,8 +173,8 @@ class Auth extends CI_Controller {
 		    $mail->isSMTP();                                            //Send using SMTP
 		    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 		    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-		    $mail->Username   = 'sakuline.banjarnegara@gmail.com';                     //SMTP username
-		    $mail->Password   = 'ppualrqhhwdjtccs';                               //SMTP password
+		    $mail->Username   = '';                     //SMTP username
+		    $mail->Password   = '';                               //SMTP password
 		    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
 		    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
